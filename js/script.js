d250b89aa4485906ce4cabd21869752183b6c46a
philmon43 <philmon43@gmail.com>
2021-02-26 18:32:15 +0200

    able move elemnt twice

--- js/script.js
+++ js/script.js
@@ -141,6 +141,7 @@ const game = (() => {
             //changing inventory bacj background image
             try {
                 el.classList.add(mineCraft.inventory[0].getAttribute("data-name"))
+                el.setAttribute("data-name", mineCraft.inventory[0].getAttribute("data-name"));
                 mineCraft.inventory.shift();
                 myInventory.style.backgroundImage = `url("./img/${mineCraft.inventory[0].getAttribute("data-name")}.png")`
                 myInventory.style.backgroundSize = "cover"
@@ -163,5 +164,7 @@ const game = (() => {
                 reset.restart()
                 break
         }
+
+        console.log(e.target)
     });
 })();
